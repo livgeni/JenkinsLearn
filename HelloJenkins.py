623da41da9ea55095b77acc6255d6ee317ed4b1e
Ivgi Livshits <liv.geni@gmail.com>
2019-05-10 11:17:28 +0300

    done editing

---
+++ HelloJenkins.py
@@ -0,0 +1,8 @@
+"""
+Add a current datetime to a file in c:\Temp
+"""
+from datetime import datetime
+
+with open('C:\Temp\HelloJenkins.txt', 'a') as file:
+    file.write(f'Current date time: {datetime.now()}')
+
